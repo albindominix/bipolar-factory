6fe4ad1c5deb56b240d15831e2e01fbd8e7832a5
Albin Antony <albinantonydominic@gmail.com>
2023-11-19 21:30:32 +0530

    first commit

---
+++ components/Page.jsx
@@ -0,0 +1,26 @@
+"use client"
+import React from "react";
+import Header from "./Header";
+import { Box, Container } from "@mui/material";
+import HeroBanner from "./HeroBanner";
+
+function Page() {
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        paddingTop: "72px",
+        pb: { xs: 8, md: 10 },
+        background: "#FBFCFD",
+      }}
+    >
+      <Container fixed>
+        <Header />
+        <HeroBanner />
+      </Container>
+      
+    </Box>
+  );
+}
+
+export default Page;
